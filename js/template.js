8136f6654381d6bcfd29cefe1590090187fe9307
Shinsuke Nakawaki <shinsuke@woodrof.com>
2022-01-06 21:58:01 +0900

    fix

--- js/template.js
+++ js/template.js
@@ -142,9 +142,10 @@ outai_template.prototype = {
 	// 使い回し用関数群
 	commonFunc: {
 		addListener: function(elm, ev, listener) {
-			/*if (!!elm.addEventListener) {
+			console.log(elm.addEventListener);
+			if (elm.addEventListener) {
 				elm.addEventListener(ev, listener, false);
-			} else */ if (elm.attachEvent) {
+			} else if (elm.attachEvent) {
 				elm.attachEvent('on' + ev, listener);
 			} else {
 				throw new Error('イベントリスナーに未対応です');
